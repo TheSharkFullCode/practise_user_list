b1a8da7d815a3ce2992b2b049a5b6f6f59b5ad99
TheSharkFullCode <agedjoscar@hotmail.com>
2023-07-26 21:17:49 +0200

    exercice

--- utils/createUser.js
+++ utils/createUser.js
@@ -3,8 +3,37 @@
 // archivo "users.js", lo añadirá AL PRINCIPIO de la lista de usuarios
 // y devolverá la nueva lista
 
-function createUser() {
-    
-}
+
+
 
 export default createUser;
+function createUser(name,lastname,age,country,holdUsers) {
+     let newId = holdUsers.length + 1
+    let newUser = {
+        first_name: name,
+        last_name: lastname,
+        age: age,
+        country: country,
+        id: newId,
+    }
+
+    console.log(newUser);
+
+    holdUsers.unshift(newUser)
+    console.log(holdUsers);
+    return holdUsers;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utils/deleteUser.js
+++ utils/deleteUser.js
@@ -1,7 +1,10 @@
 // La función "deleteUser" debe buscar el usuario dentro de la lista,
 // eliminarlo, y devolver la nueva lista sin el usuario en cuestión
 
-function deleteUser() {
+import Users from "../users";
+
+function deleteUser(deleteName, holdUsers) {
+    const deleteName = deleteName.filter()
 
 }
 
